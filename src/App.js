097f72cd4825de8b74ad5f3fd7a37493f6241b36
Kevin <kevcespedesarancibia87@gmail.com>
2021-08-26 10:32:07 -0400

    Hola nombre + edad

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
 const nombre = document.querySelector("#nombre-input");
 const edad = document.querySelector("#edad-input");
 const form = document.querySelector("#saludador-form");
+
+
+/*function genderSelect(gender){
+    if(gender=="masculino")
+    {
+        var greeting = ""
+    }
+}
+function greetingSelector(name,age,gender) {
+    if (age<25){
+        
+        
+    }else if{
+        alert("Buenos dias mi lord "+nombre.value);
+    }
+}*/
+
 form.addEventListener("submit",event=>{
     if (edad.value<25){
         alert("Que hay de nuevo "+nombre.value);
