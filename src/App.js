9e10813e1476301894a2816f1c755db7f486a5f6
Kevin <kevcespedesarancibia87@gmail.com>
2021-08-26 10:02:17 -0400

    Hola nombre + edad

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 const nombre = document.querySelector("#nombre-input");
+const edad = document.querySelector("#edad-input");
 const form = document.querySelector("#saludador-form");
 form.addEventListener("submit",event=>{
-    alert("Hola "+nombre.value);
+    if (edad.value<25){
+        alert("Que hay de nuevo "+nombre.value);
+    }else{
+        alert("Buenos dias mi lord "+nombre.value);
+    }
 })
